0c40177e15cc9bdc2719b6d44edac3bf05e2a12c
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-03-31 16:00:41 -0500

    connecting the server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
 
 function App() {
+  // use dispatch
+  const dispatch = useDispatch();
+
+  // on page load
+  useEffect(() => {
+    dispatch({type:'FETCH_OWNER_LIST'});
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <h1>testing stuff for now</h1>
+   </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+import axios from 'axios';
+import {takeEvery, put} from 'redux-saga/effects';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function* rootSaga (action) {
+  yield takeEvery('FETCH_OWNER_LIST', fetchOwnerList)
+}; // end rootSaga
+
+function* fetchOwnerList () {
+  try {
+    const owners = yield axios.get('/api/owners/all');
+
+    yield put({type: 'SET_OWNER_LIST', payload: owners.data})
+  }
+  catch (err) {
+    console.log('something screwed up 💥', err);
+  }
+}
+
+const ownerList = function (state = [], action) {
+  switch (action.type) {
+    case 'SET_OWNER_LIST':
+      return action.payload;
+    default:
+      return state;
+  }
+}; // end ownerList
+
+const sagaMiddleware = createSagaMiddleware();
+
+const storeInstance = createStore(
+  combineReducers({
+      ownerList,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={storeInstance}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
