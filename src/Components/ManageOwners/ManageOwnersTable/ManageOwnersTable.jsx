0714ed8ce14f6189b8961ba86fb40fcc84502a71
patrickrnelson <patrickrnelson1@gmail.com>
2021-04-01 14:04:21 -0500

    setting up delete communication

--- src/Components/ManageOwners/ManageOwnersTable/ManageOwnersTable.jsx
+++ src/Components/ManageOwners/ManageOwnersTable/ManageOwnersTable.jsx
@@ -1,12 +1,17 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './ManageOwnersTable.css'
 
 function ManageOwnersTable() {
+  const dispatch = useDispatch();
 
   const owners = useSelector(store => store.ownerList);
 
-  const handleDelete = () => {
-    console.log('DELETE CLICK');
+  const handleDelete = (id) => {
+    console.log('DELETE CLICK. id:', id);
+    dispatch({
+      type: 'DELETE_OWNER',
+      payload: id
+    })
   }
 
   return (
@@ -21,14 +26,14 @@ function ManageOwnersTable() {
           </tr>
         </thead>
         <tbody>
-          {owners.map(owner => { 
+          {owners.map((owner, index) => { 
             return (
-              <tr key={owner[1]}>
+              <tr key={index}>
                 <td>{owner[2]}</td>
                 <td>{owner[3]}</td>
                 <td>{owner[0]}</td>
                 <td>
-                  <button onClick={handleDelete}>DELETE</button>
+                  <button onClick={() => handleDelete(owner[1])}>DELETE</button>
                 </td>
               </tr>
             )
